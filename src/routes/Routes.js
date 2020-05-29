25508095d33e31ca320355b5ba740e290f05cfa5
chenghongwei <15933111693@163.com>
2020-05-29 14:01:28 +0800

    加入了页面缓存

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Provider } from 'react-keep-alive'
 import routes from './index'
 
 const Routes = () => {
     return (
         <Router>
-            {routes.map((route, index) => (
-                <Route exact path={route.path} component={route.component} key={index}/>
-            ))}
+            <Provider>
+                {routes.map((route, index) => (
+                    <Route exact path={route.path} component={route.component} key={index}/>
+                ))}
+            </Provider>
         </Router>
     );
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,19 @@
+import React from 'react'
+import { KeepAlive } from 'react-keep-alive'
 import App from '../App'
 import Login from '../pages/login/index'
 
 const routeConfig = [
-    {path:'/', component: App},
-    {path:'/login', component: Login},
+    {path:'/', component: () => (
+        <KeepAlive name='index'>
+            <App/>
+        </KeepAlive>
+    )},
+    {path:'/login', component: () => (
+        <KeepAlive name='login'>
+            <Login/>
+        </KeepAlive>
+    )},
 ]
 
 export default routeConfig
